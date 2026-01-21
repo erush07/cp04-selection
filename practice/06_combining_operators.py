4d2352d40c844873745bc6b13a404caafb25e35a
jacobsteffenBYU <jacob.steffen@byu.edu>
2026-01-21 11:26:05 -0700

    created new example for the combining operators

--- practice/06_combining_operators.py
+++ practice/06_combining_operators.py
@@ -23,65 +23,46 @@ Use parentheses to group conditions together to make sure they are
 evaluated correctly
 '''
 
-# SCENARIO FOR EXAMPLES BELOW:
-# A student can win a prize based on a few combinations of conditions.
-# We have 4 conditions we will evaluate:
-#   - grades_above_90
-#   - attendance_above_90
-#   - extracurriculars
-#   - no_behavior_issues
 
-# Rules:
-#   - No student can win a prize if they have behavior issues.
-#   - A student can win a prize if they have grades_above_90 OR
-#     if they have both attendance_above_90 AND extracurriculars.
+# 1. SCENARIO
+'''
+For orders, customers should receive free shipping if the order is not flagged
+and if either the total is over $50 or they have a FREESHIPPING coupon
+Free shipping if the order is not flagged, and (total ≥ 50 OR coupon is “FREESHIP”).
+'''
+# Using the variables below create an if/else statement that prints "FREE SHIPPING"
+# if the order should get free shipping, or "NO FREE SHIPPING" if the order
+# shouldn't get free shipping
 
-# SITUATIONS:
-# 1: A student has everything they need, except they have behavior issues.
-# 2: A student meets the attendance and extracurricular condition,
-#    but has behavior issues.
-# 3: A student meets the attendance condition, and has no behavior issues.
-# 4: A student meets the attendance condition and extracurricular option,
-# and has no behavior issues.
+order_total = 20
+coupon = "FREESHIPPING"
+flagged = False
 
+
+
+
+# 2. SCENARIO:
+'''
+A student can win a prize based on a few combinations of conditions.
+We have 4 conditions we will evaluate:
+  - grades_above_90
+  - attendance_above_90
+  - extracurriculars
+  - no_behavior_issues
+
+Rules:
+  - No student can win a prize if they have behavior issues.
+  - A student can win a prize if they have grades_above_90 OR
+    if they have both attendance_above_90 AND extracurriculars.
+'''
+
+# SITUATION:
+# A student has everything they need, except they have behavior issues.
 grades_above_90 = True
 attendance_above_90 = True    
 extracurriculars = True  
 no_behavior_issues = False   
 
-# S1.1 - WITHOUT PARENTHESES: 
-# This is written without parentheses separating the conditions.
-# Stare at it for a minute. Is it working correctly?
-
-if (no_behavior_issues == True and grades_above_90 == True
-    or attendance_above_90 == True and extracurriculars == True):
-    print("ex1.1: Student wins the prize! (incorrect logic)")
-else:
-    print("ex1.1: Student does not win the prize. (incorrect logic)")
-
-'''
-TIP
----
-Yes there are parentheses at the beginning and end of the if conditions.
-That is to make it so I can put the conditions on multiple lines of code.
-The point here is that there aren't parentheses separating the
-various conditions
-'''
-
-
-# 1. MATCH IF STATEMENT TO NEW LOGIC:
-# Let's change the rules:
-# Make it so a student can win a prize as long as they have at least one of:
-#   - grades above 90
-#   - attendance above 90
-#   - extracurricular activities.
-# But they still can't have behavioral issues in any case.
-# evaluate the student below and write an if statement that accurately 
-# reflects this logic:
-
-grades_above_90 = False
-attendance_above_90 = False
-extracurriculars = True
-no_behavior_issues = True
-
+# using the rules above, write the logic that would print out "Student wins
+# a prize!" if they should win, or "student does not win a prize" if they shouldn't
 
